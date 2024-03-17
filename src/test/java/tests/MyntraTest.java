6c9366edff1187068cdf53c9990edc600e5b6a82
mbm777 <balamuralim111@gmail.com>
2024-03-17 16:27:16 +0530

    pom.xml changed

--- src/test/java/tests/MyntraTest.java
+++ src/test/java/tests/MyntraTest.java
@@ -45,8 +45,7 @@ public class MyntraTest {
 			options.addArguments("--ignore-ssl-errors=yes");
 			options.addArguments("--ignore-certificate-errors");
 			options.addArguments("--headless"); //should be enabled for Jenkins
-			//options.addArguments("--disable-dev-shm-usage"); //should be enabled for Jenkins
-			//options.addArguments("--window-size=1920x1080"); //should be enabled for Jenkins
+			options.addArguments("--window-size=1920x1080"); //should be enabled for Jenkins
 			//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/executables/chromedriver");
 			driver = new ChromeDriver(options);
 		} else if (browserName.equalsIgnoreCase("edge")) {
